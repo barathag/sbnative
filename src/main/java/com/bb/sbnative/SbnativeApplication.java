4aa932a963ab541ebbf22fed855af2f1d82a9201
barathag <barath.1902016@srit.org>
2024-01-17 17:38:28 +0530

    first commit

---
+++ src/main/java/com/bb/sbnative/SbnativeApplication.java
@@ -0,0 +1,13 @@
+package com.bb.sbnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbnativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbnativeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bb/sbnative/SbnativeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bb.sbnative;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbnativeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
